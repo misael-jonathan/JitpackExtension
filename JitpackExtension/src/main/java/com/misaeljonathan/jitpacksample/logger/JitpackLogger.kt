57c84535e7567f1ae2275f41914749d85af7069d
Misael Jonathan <misael.jonathan@tokopedia.com>
2023-05-18 21:03:29 +0700

    Init repository

---
+++ JitpackExtension/src/main/java/com/misaeljonathan/jitpacksample/logger/JitpackLogger.kt
@@ -0,0 +1,16 @@
+package com.misaeljonathan.jitpacksample.logger
+
+import android.util.Log
+
+object JitpackLogger {
+
+    val TAG = "JitpackSample19May"
+
+    fun sendLog(message: String) {
+        Log.d(TAG, message)
+    }
+
+    fun sendLog(tag:String, message: String) {
+        Log.d(tag, message)
+    }
+}
